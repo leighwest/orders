a31a7ff86c76c87180443cd4735fed5c28121e13
Leigh West <leighwest@hotmail.com>
2023-12-27 20:30:00 +0000

    Fixed error where tests were using mysql db instead of h2

--- src/test/java/com/west/orders/repository/OrderRepositoryTest.java
+++ src/test/java/com/west/orders/repository/OrderRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles(profiles = "test")
 class OrderRepositoryTest {
 
     @Autowired
