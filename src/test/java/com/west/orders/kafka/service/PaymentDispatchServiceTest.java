210cb500bdfb48ea6db71521cad286571e6b808f
Leigh West <leighwest@hotmail.com>
2023-12-27 20:07:51 +0000

    Minor type amendment to test

--- src/test/java/com/west/orders/kafka/service/PaymentDispatchServiceTest.java
+++ src/test/java/com/west/orders/kafka/service/PaymentDispatchServiceTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.times;
 
 class PaymentDispatchServiceTest {
 
-    private KafkaTemplate kafkaProducerMock;
+    private KafkaTemplate<String, Object> kafkaProducerMock;
 
     private PaymentDispatchService paymentDispatchService;
 
