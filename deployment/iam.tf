075681e8a507b6f393145754e5d3ee38fb2a7386
Leigh West <leighwest@hotmail.com>
2024-04-04 21:06:29 +0000

    Terraform IAM config for SES

---
+++ deployment/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_user" "smtp_user" {
+  name = "ses-smtp-user"
+}
+
+resource "aws_iam_user_policy_attachment" "ses_send_policy" {
+  user       = aws_iam_user.smtp_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+}
+
+resource "aws_iam_access_key" "smtp_credentials" {
+  user = aws_iam_user.smtp_user.name
+}
+
+resource "aws_ssm_parameter" "smtp_username" {
+  name  = "orders_smtp_username"
+  type  = "String"
+  value = aws_iam_access_key.smtp_credentials.id
+}
+
+resource "aws_ssm_parameter" "smtp_password" {
+  name  = "orders_smtp_password"
+  type  = "SecureString"
+  value = aws_iam_access_key.smtp_credentials.secret
+}
