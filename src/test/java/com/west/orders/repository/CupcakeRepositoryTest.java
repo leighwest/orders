2cd80efa7d9fc7f6846378d7e3246ebbcb8143f8
Leigh West <leighwest@hotmail.com>
2026-06-05 09:11:57 +1000

    Fix test

--- src/test/java/com/west/orders/repository/CupcakeRepositoryTest.java
+++ src/test/java/com/west/orders/repository/CupcakeRepositoryTest.java
@@ -21,12 +21,15 @@ class CupcakeRepositoryTest {
     @Autowired
     private CupcakeRepository cupcakeRepository;
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     @Test
     public void shouldReturn_cupcake_whenFindByProductCode() {
 
-        Image chocolateCupcakeImage = new Image(null, "CHOC001", "bucketName", "objectKey");
+        Image savedImage = imageRepository.save(new Image(null, "CHOC001", "bucketName", "objectKey"));
         Cupcake chocolateCupcake = new Cupcake(null, "CHOC001", Cupcake.Flavour.CHOCOLATE,
-                BigDecimal.valueOf(3.50), chocolateCupcakeImage);
+                BigDecimal.valueOf(3.50), savedImage);
         Cupcake savedCupcake = cupcakeRepository.save(chocolateCupcake);
 
         Cupcake found = cupcakeRepository.findByProductCode(savedCupcake.getProductCode());
